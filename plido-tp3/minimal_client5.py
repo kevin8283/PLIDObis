e78150f7628ef7bd864314d323a7c959ce968144
Kevin <keyvinkeygen@gmail.com>
2022-06-30 15:02:58 +0200

    8th question lab2

--- plido-tp3/minimal_client5.py
+++ plido-tp3/minimal_client5.py
@@ -1,19 +1,31 @@
 from virtual_sensor import virtual_sensor 
 import time
+import os
 import socket
 import cbor2 as cbor
-
-temperature = virtual_sensor(start=20, variation = 0.1)
-pressure    = virtual_sensor(start=1000, variation = 1) 
-humidity    = virtual_sensor(start=30, variation = 3, min=20, max=80) 
+    
+humidity = virtual_sensor(start=30, variation = 3, min=20, max=80) 
  
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 while True:
-    t = int(temperature.read_value() * 100)
-    p = int(pressure.read_value() * 100)
-    h = int(humidity.read_value() * 100)
+    humidity_values = []
 
-    j = [t, p, h]
-    s.sendto (cbor.dumps(j), ("127.0.0.1", 33033))
-    time.sleep(2)
+    time_start = time.time()
+    elapsed_time = time.time()
+
+    while elapsed_time - time_start <= 10:
+        number_of_values = len(humidity_values)
+        print("Collecting humidity values ... (current values obtained: {})".format(number_of_values))
+
+        h = humidity.read_value()
+        humidity_values.append(int(h * 100) / 100)
+        time.sleep(1)
+        os.system('clear')
+
+        elapsed_time = time.time()
+
+    print("10 seconds elapsed, sending {} values".format(len(humidity_values)))
+    time.sleep(1)
+    s.sendto(cbor.dumps(humidity_values),("127.0.0.1", 33033))
+    os.system('clear')
--- plido-tp3/minimal_server.py
+++ plido-tp3/minimal_server.py
@@ -9,35 +9,9 @@ os.system("clear")
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.bind(('0.0.0.0', 33033))
 
-temperature_values = []
-pressure_values = []
-humidity_values = []
-
-def calculate_average(array):
-    sum = 0
-    for value in array:
-        sum += value
-   
-    return sum / len(array)
-
 while True:
     data, addr = s.recvfrom(1500)
    
-    captor_values = cbor.loads(data)
+    humidity_values = cbor.loads(data)
 
-    temperature_values.append(captor_values[0] / 100)
-    pressure_values.append(captor_values[1] / 100 )
-    humidity_values.append(captor_values[2] / 100 )
-
-    avg_temp = calculate_average(temperature_values)
-    avg_press = calculate_average(pressure_values)
-    avg_hum = calculate_average(humidity_values)
-
-    print("Average values from captor: ")
-    print("")
-    print("Temperature: ", avg_temp)
-    print("Pressure: ", avg_press)
-    print("Humidity: ", avg_hum)
-
-    time.sleep(2)
-    os.system('clear')
+    print(humidity_values)
